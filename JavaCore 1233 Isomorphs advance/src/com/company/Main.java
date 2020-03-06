cec84f937e1fd1c947d20489049b93c088ba6f84
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-06 13:59:09 +0200

    Add files via upload

---
+++ JavaCore 1233 Isomorphs advance/src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+/*
+
+1233 Isomorphs advance
+Write a method that returns the minimum number in the array and its position (index).
+
+Requirements:
+1. The Solution class must contain the Pair class.
+2. The Solution class must contain two methods.
+3. The Solution class must contain the getMinimumAndIndex () method.
+4. The getMinimumAndIndex () method should return the minimum number in the array and its position (index).
+
+
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Index of minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        int min = array[0];
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return new Pair<Integer, Integer>(min, index);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+
+
